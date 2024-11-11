e1a3ed88382a0ce67a89634cf0008c1124e53b29
Doug Inman <douglas.inman@live5.co.uk>
2024-11-11 00:22:43 +0000

    cleanup wave structure. added basic waves

--- src/scene_game.hpp
+++ src/scene_game.hpp
@@ -92,11 +92,10 @@ struct SceneGame : Scene {
 	vector<int> bullets;
 	int bulletcd = 0;
 	// enemys
-	// vector<int> enemys;
-	Wave1 wave = { gfx };
+	Wave1 wave = { { gfx } };
 	// effects & interface
 	StarField starfield;
-	// Effects::Fade fade;
+	Effects::Fade fade;
 	Interface interface;
 
 
@@ -127,9 +126,7 @@ struct SceneGame : Scene {
 			if (gfx.collidesprite( bullet )) {
 				collide++;
 				for (int c : gfx.collisions_sprite)
-					// enemykill( c );
 					wave.kill( c );
-					// ;
 			}
 			// erase collided bullets
 			if (collide) {
@@ -151,16 +148,6 @@ struct SceneGame : Scene {
 		}
 
 		// move enemys
-		// for (int i = enemys.size() - 1; i >= 0; i--) {
-		// 	int enemyid = enemys[i];
-		// 	if (enemyupdate( enemyid )) {
-		// 		gfx.freesprite( enemyid );
-		// 		enemys.erase( enemys.begin() + i );
-		// 	}
-		// }
-		// // spawn new enemys
-		// if (enemys.size() < 10 && rand() % 10 == 0)
-		// 	enemys.push_back( enemymake_wobble() );
 		wave.update();
 	}
 
@@ -169,65 +156,6 @@ struct SceneGame : Scene {
 		gfx.drawscene();
 		interface.drawscene();
 	}
-
-	// struct SentryData { char alive; };
-	// int enemymake() {
-	// 	int enemyid = gfx.makesprite( tilesetimage, TSIZE, TSIZE );
-	// 	auto& enemy = gfx.getsprite( enemyid );
-	// 	enemy.src.x = TSIZE * 4;
-	// 	enemy.pos.x = 2 + ( rand() % (Scene::SCENEW - TSIZE - 4) );
-	// 	enemy.pos.y = -TSIZE;
-	// 	// enemy data
-	// 	enemy.usertype = 1;
-	// 	// enemy.userdata.push_back( true );
-	// 	enemy.userdata.resize( sizeof(SentryData) );
-	// 	auto& data = (SentryData&)enemy.userdata[0];
-	// 	data.alive = true;
-	// 	return enemyid;
-	// }
-
-	// struct SentryWobbleData : SentryData { double x, y, xspeed, yspeed, xacc; };
-	// int enemymake_wobble() {
-	// 	int enemyid = enemymake();
-	// 	auto& enemy = gfx.getsprite( enemyid );
-	// 	// enemy data
-	// 	enemy.usertype = 2;
-	// 	enemy.userdata.resize( sizeof(SentryWobbleData) );
-	// 	auto& data = (SentryWobbleData&)enemy.userdata[0];
-	// 	data.x = enemy.pos.x;
-	// 	data.y = enemy.pos.y;
-	// 	data.yspeed = 1;
-	// 	data.xacc = 0.07;
-	// 	return enemyid;
-	// }
-
-	// bool enemyupdate(int enemyid) {
-	// 	auto& enemy = gfx.getsprite( enemyid );
-	// 	auto& data = (SentryData&)enemy.userdata[0];
-	// 	// straight down enemy
-	// 	if (enemy.usertype == 1) {
-	// 		enemy.pos.y += 1;
-	// 	}
-	// 	// wobble enemy
-	// 	if (enemy.usertype == 2) {
-	// 		auto& data = (SentryWobbleData&)enemy.userdata[0];
-	// 		data.xspeed += data.xacc;
-	// 		if (abs(data.xspeed) >= 1.0)  data.xacc = -data.xacc;
-	// 		data.x += data.xspeed;
-	// 		data.y += data.yspeed;
-	// 		enemy.pos.x = round(data.x);
-	// 		enemy.pos.y = round(data.y);
-	// 	}
-	// 	// check offscreen, return true if dead
-	// 	if (enemy.pos.y > SCENEH)
-	// 		data.alive = false;
-	// 	return !data.alive;
-	// }
-
-	// void enemykill(int enemyid) {
-	// 	auto& enemy = gfx.getsprite( enemyid );
-	// 	enemy.userdata[0] = false;
-	// }
 };
 
 extern SceneGame game;
--- src/waves.hpp
+++ src/waves.hpp
@@ -1,34 +1,26 @@
 #pragma once
 #include "global.hpp"
+#include <cmath>
 
-struct Wave1 {
-	static const int TSIZE = Scene::TSIZE;
+struct Wave {
+	enum ENEMY_T {
+		ENEMY_ORB = 200,
+		ENEMY_WOBBLEORB = 201,
+	};
 	struct SentryData { char alive; };
 	struct SentryWobbleData : SentryData { double x, y, xspeed, yspeed, xacc; };
 
+	static const int TSIZE = Scene::TSIZE;
 	GFX::Scene& gfx;
 	vector<int> enemys;
-	int delta;
+	int delta = 0;
 
 	void update() {
 		delta++;
 		// move enemys
 		for (int i = (int)enemys.size()-1; i >= 0; i--)
-			if (enemyupdate( enemys[i] ))
+			if (updateenemy( enemys[i] ))
 				enemys.erase( enemys.begin() + i );
-		// spawn next enemy
-		switch (delta) {
-		case 1:
-		case 20:
-		case 40:
-		case 60:
-		case 80:
-			enemymake( 30 );
-			break;
-		case 200:
-			delta = 0;
-			break;
-		}
 	}
 
 	void kill(int enemyid) {
@@ -39,7 +31,7 @@ struct Wave1 {
 		}
 	}
 
-	int enemymake(int x) {
+	int makeorb(int x) {
 		int enemyid = gfx.makesprite( Scene::tilesetimage, TSIZE, TSIZE );
 		auto& enemy = gfx.getsprite( enemyid );
 		enemy.src.x = TSIZE * 4;
@@ -47,7 +39,7 @@ struct Wave1 {
 		enemy.pos.x = x;
 		enemy.pos.y = -TSIZE;
 		// enemy data
-		enemy.usertype = 1;
+		enemy.usertype = ENEMY_ORB;
 		enemy.userdata.resize( sizeof(SentryData) );
 		// auto& data = (SentryData&)enemy.userdata[0];
 		// data.alive = true;
@@ -55,40 +47,69 @@ struct Wave1 {
 		return enemyid;
 	}
 
-	// int enemymake_wobble() {
-	// 	int enemyid = enemymake();
-	// 	auto& enemy = gfx.getsprite( enemyid );
-	// 	// enemy data
-	// 	enemy.usertype = 2;
-	// 	enemy.userdata.resize( sizeof(SentryWobbleData) );
-	// 	auto& data = (SentryWobbleData&)enemy.userdata[0];
-	// 	data.x = enemy.pos.x;
-	// 	data.y = enemy.pos.y;
-	// 	data.yspeed = 1;
-	// 	data.xacc = 0.07;
-	// 	return enemyid;
-	// }
-
-	bool enemyupdate(int enemyid) {
+	int makewobbleorb(int x) {
+		int enemyid = makeorb( x );
 		auto& enemy = gfx.getsprite( enemyid );
-		// auto& data = (SentryData&)enemy.userdata[0];
+		// enemy data
+		enemy.usertype = ENEMY_WOBBLEORB;
+		enemy.userdata.resize( sizeof(SentryWobbleData) );
+		auto& data = (SentryWobbleData&)enemy.userdata[0];
+		data.x = enemy.pos.x;
+		data.y = enemy.pos.y;
+		data.yspeed = 1;
+		data.xacc = 0.07;
+		return enemyid;
+	}
+
+	bool updateenemy(int enemyid) {
+		auto& enemy = gfx.getsprite( enemyid );
 		// straight down enemy
-		if (enemy.usertype == 1) {
+		if (enemy.usertype == ENEMY_ORB) {
+			// auto& data = (SentryData&)enemy.userdata[0];
 			enemy.pos.y += 1;
 		}
 		// wobble enemy
-		// if (enemy.usertype == 2) {
-		// 	auto& data = (SentryWobbleData&)enemy.userdata[0];
-		// 	data.xspeed += data.xacc;
-		// 	if (abs(data.xspeed) >= 1.0)  data.xacc = -data.xacc;
-		// 	data.x += data.xspeed;
-		// 	data.y += data.yspeed;
-		// 	enemy.pos.x = round(data.x);
-		// 	enemy.pos.y = round(data.y);
-		// }
+		if (enemy.usertype == ENEMY_WOBBLEORB) {
+			auto& data = (SentryWobbleData&)enemy.userdata[0];
+			data.xspeed += data.xacc;
+			if (abs(data.xspeed) >= 1.0)  data.xacc = -data.xacc;
+			data.x += data.xspeed;
+			data.y += data.yspeed;
+			enemy.pos.x = round(data.x);
+			enemy.pos.y = round(data.y);
+		}
 		// check offscreen, return true if dead
 		if (enemy.pos.y > Scene::SCENEH)
 			return true;
 		return false;
+	}
+};
+
+
+struct Wave1 : Wave {
+	void update() {
+		Wave::update();
+		// spawn next enemy
+		switch (delta) {
+		case 1:
+		case 20:
+		case 40:
+		case 60:
+		case 80:
+			makeorb( 15 );
+			break;
+		case 200:
+		case 215:
+		case 230:
+		case 245:
+		case 260:
+		case 275:
+		case 290:
+			makewobbleorb( (Scene::SCENEW - TSIZE) / 2 );
+			break;
+		case 400:
+			delta = 0;
+			break;
+		}
 	}
 };
